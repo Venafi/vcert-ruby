3d916aea15a3f0fc7b7a9165ee5646caf36a5974
Denis Subbotin <denis@subbot.in>
2019-09-20 13:30:27 +0300

    skeleton tpp

--- lib/vcert.rb
+++ lib/vcert.rb
@@ -21,4 +21,14 @@ module Vcert
 
     end
   end
+
+
+  class TPPConnection
+    def initialize(url, user, password)
+      @url = url
+      @user = user
+      @password = password
+    end
+  end
+
 end
