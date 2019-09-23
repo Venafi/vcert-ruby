64487de17d8288c089b9c66b6c7184f0bb1db4e5
Denis Subbotin <denis@subbot.in>
2019-09-23 17:39:35 +0300

    connections selector

--- lib/vcert.rb
+++ lib/vcert.rb
@@ -1,19 +1,22 @@
 require 'net/https'
 
 
-class Vcert
-
+module Vcert
   class Connection
-
-    def initialize(url, token)
-      @conn = CloudConnection.new url, token
+    def initialize(url=nil, user=nil, password=nil, cloud_token=nil,)
+      if cloud_token != nil then
+        @conn = CloudConnection.new url, token
+      elsif user != nil && password != nil && url != nil then
+        @conn = TPPConnection.new url, user, password
+      else
+        raise "Invalid credentials list" # todo: add typed exceptions
+      end
     end
 
     def request(*args)
       @conn.request(*args)
     end
   end
-
 end
 
 
