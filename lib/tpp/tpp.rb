cfd27f1540335b3df3d7e22ad313e0d6f39ff998
Denis Subbotin <denis@subbot.in>
2019-10-09 13:14:25 +0300

    parse pem list

--- lib/tpp/tpp.rb
+++ lib/tpp/tpp.rb
@@ -121,4 +121,26 @@ class Vcert::TPPConnection
   def parse_full_chain(full_chain)
     Vcert::Certificate.new  full_chain, '', nil # todo: parser
   end
+
+  def parse_pem_list(multiline)
+    pems = []
+    buf = ""
+    current_string_is_pem = false
+    multiline.each_line do |line|
+      if line.match(/-----BEGIN [A-Z]+-----/)
+        current_string_is_pem = true
+      end
+      if current_string_is_pem
+        buf = buf + line
+      end
+      if line.match(/-----END [A-Z]+-----]/)
+        current_string_is_pem = false
+        pems.push(buf)
+        buf = ""
+      end
+    end
+    pems
+  end
 end
+
+
